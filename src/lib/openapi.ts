ba45abcfd1e131cb3c6a14c027ac11bc732d0e64
buttondown <engineering@buttondown.email>
2026-01-20 03:25:12 +0000

    Update from https://github.com/buttondown/monorepo/commit/e96dd0e2383b0b2d93dc1372731cd0132cf8ff3b

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -4235,7 +4235,7 @@ export interface components {
      * @description An enumeration. 
      * @enum {string}
      */
-    EmailEventType: "activation_clicked" | "activation_delivered" | "activation_opened" | "activation_bounced" | "activation_rejected" | "activation_complained" | "activation_reminder_clicked" | "activation_reminder_delivered" | "activation_reminder_opened" | "activation_reminder_bounced" | "activation_reminder_complained" | "activation_reminder_rejected" | "subscription_confirmed_clicked" | "subscription_confirmed_delivered" | "subscription_confirmed_opened" | "subscription_confirmed_bounced" | "subscription_confirmed_rejected" | "subscription_confirmed_complained" | "attempted" | "bounced" | "complained" | "clicked" | "delivered" | "opened" | "sent" | "rejected" | "replied" | "unsubscribed";
+    EmailEventType: "activation_bounced" | "activation_clicked" | "activation_complained" | "activation_deferred" | "activation_delivered" | "activation_opened" | "activation_rejected" | "activation_reminder_bounced" | "activation_reminder_clicked" | "activation_reminder_complained" | "activation_reminder_deferred" | "activation_reminder_delivered" | "activation_reminder_opened" | "activation_reminder_rejected" | "subscription_confirmed_clicked" | "subscription_confirmed_bounced" | "subscription_confirmed_complained" | "subscription_confirmed_deferred" | "subscription_confirmed_delivered" | "subscription_confirmed_opened" | "subscription_confirmed_rejected" | "attempted" | "bounced" | "clicked" | "complained" | "deferred" | "delivered" | "opened" | "rejected" | "replied" | "sent" | "unsubscribed";
     /** EmailEvent */
     EmailEvent: {
       /**
